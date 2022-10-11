87bff07a0cf7a119eb6c7544774a9f8df3b1f42d
HawraaYounes <hawraayounes0@gmail.com>
2022-10-11 10:58:02 +0300

    backend: Finishing addCourse api

--- backend/app/Http/Controllers/CourseController.php
+++ backend/app/Http/Controllers/CourseController.php
@@ -7,5 +7,16 @@ use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
-    //
+    function addCourse(Request $request){
+        $course = Course::create([
+            'code'=>$request->code,
+            'name'=>$request->name,
+           
+        ]);
+        $course->save();
+        return response()->json([
+            "status" => "Success",
+            "data" => $course
+        ]);
+    }
 }
